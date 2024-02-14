1ffb20836d72da82308db38633f0148723bb2d88
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-14 02:47:48 +0200

    Server SetUp

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # Write a Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+apt-get update
+apt-get -y install nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-echo "<h1>Abimbola Ronald</h1>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+echo "<h1>Abimbola Ronald</h1>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
