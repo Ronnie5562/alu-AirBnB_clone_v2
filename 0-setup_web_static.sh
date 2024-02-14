30e5c2afc0223f262f6979b8e755c574e45d050a
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-14 02:43:11 +0200

    Server SetUp

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,4 +11,4 @@ echo "<h1>Abimbola Ronald</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-service nginx reload
+service nginx restart
