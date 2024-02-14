aac0cafe85099f45af8dc03bfe2fc4317d999edc
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-14 20:18:59 +0200

    Created: Wrote a fabric script that generates a .tgz file from web_static dir

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,14 +2,12 @@
 """
 Fabric script that generates a .tgz file from the contents of the web_static
 """
-
 from fabric.api import local
 from time import strftime
 
 
 def do_pack():
-    """
-    Generates a .tgz file from the contents of the web_static
+    """ Generates a .tgz file from the contents of the web_static
     """
 
     date_time = strftime("%Y%m%d%H%M%S")
@@ -17,5 +15,6 @@ def do_pack():
     try:
         local("mkdir -p versions")
         local(f"tar -zcvf {file_name} web_static")
+        return file_name
     except Exception as err:
         return None
