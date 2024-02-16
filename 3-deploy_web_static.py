fa06ecda79cc2f2a025e1dba1294c23f867562c3
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-16 12:52:38 +0200

    A combination on 1-pack_web_static and 2-do_deploy_web_static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from os.path import join, exists, splitext
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
-
+env.hosts = ["54.242.215.110", "34.229.154.33"]
 def deploy():
     """
     Deploys the web_static content to the web servers.
