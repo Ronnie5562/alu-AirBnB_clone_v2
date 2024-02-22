48c99c22a30a5f51fc5c0e9848c20f2d94145eb8
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-23 01:28:42 +0200

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module that starts a Flask web application"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,4 +12,4 @@ def index():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
