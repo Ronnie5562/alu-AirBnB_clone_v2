50346d734e49c0a19174297c9fb60d488b5e4edd
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-23 09:08:02 +0200

    Task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Module that starts a Flask web application"""
 from flask import Flask, render_template
+from models.state import State
 from models import storage
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def states_list(n):
+def states_list():
     """Returns an HTML page with a list of all States in the database"""
     states = storage.all("State").values()
     return render_template("7-states_list.html", states=states)
